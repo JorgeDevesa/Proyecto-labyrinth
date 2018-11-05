8378ed8d119c3c3d7bfa2440894e8a52ae646f62
Jorge Devesa <jrgdevesa@gmail.com>
2018-11-05 22:31:31 +0100

    añadido el click para cambio de posición de los objetos

--- JS/game.js
+++ JS/game.js
@@ -6,46 +6,52 @@ function Game(canvasId){
     // this.obstacle = new Obstacle(this.game)
     // this.map = new Map (this.game)
     this.mapX = 11;
-    this.mapY = 13;
+    this.mapY = 15;
     this.gw = 500/this.mapX;
     this.gh = 600/this.mapY;
     this.position = {x:0, y:0};
-    // this.adjX = 22.5; //ajusta un elemento al centro de la casilla
-    // this.adjY = 25;
-    
-}
-
-    // canvas = document.getElementById("myCanvas");
-    //  ctx = canvas.getContext('2d');
-   Game.prototype.click= function(){
-  
-    this.canvas.onmouseup = function(event){
-    this.position.x = Math.floor(event.clientX / this.gw);
-    this.position.y = Math.floor(event.clientY / this.gh);
-    console.log("x: " + this.position.x + " y: " + this.position.y)
-    }.bind(this)
+    this.obstacle = {};
 }
 
 Game.prototype.start = function(){
     var counter = 1; //contador de niveles. Tengo que sumarle uno cuando llegue a la mata
     var map = new Map (this.ctx)
-    var ball = new Ball(this.ctx, map) //+ niveles
-    var obstacle = new Obstacle(this.ctx, map, ball)
+    var ball = new Ball(this.ctx) //+ niveles //puedo pasarle this para coger el parametro game
+    this.obstacle = new Obstacle(this.ctx, map, ball, this)
     var goal = new Goal(this.ctx, map, ball)
 
     setInterval(function(){
-        this.click();
+    this.click();
     this.clearAll();
     ball.moveBall();
     ball.drawBall();
-    obstacle.draw();
+    this.obstacle.draw();
     goal.draw();
-    obstacle.colapse()
+    this.obstacle.colapse()
     goal.finnish()
 
     }.bind(this), 1000/60);
     
 }
+Game.prototype.click= function(){
+  
+    this.canvas.onmouseup = function(event){
+    this.position.x = Math.floor(event.clientX/this.gw);
+    this.position.y = Math.floor(event.clientY/this.gh);
+    console.log("x: " + this.position.x + " y: " + this.position.y)
+    if(this.position.x === this.obstacle.realPosition.x && this.position.y === this.obstacle.realPosition.y){
+        if (this.obstacle.position ==="a"){
+        this.obstacle.position ="b"
+        } 
+        else {
+            this.obstacle.position ="a"
+        }
+    }
+    }.bind(this)
+   
+   
+}
+
 Game.prototype.clearAll = function(){
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
